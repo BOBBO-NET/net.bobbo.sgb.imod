8274d54a2311033e1d321a8496eb92945a973e99
LAPTOP-3BPAUQ9G\Iseei <iseeicyhawkston@gmail.com>
2023-08-10 13:06:55 -0500

    Mixer volume is set when handle provided

--- Runtime/Scripts/SGBAudioSettings.cs
+++ Runtime/Scripts/SGBAudioSettings.cs
@@ -18,6 +18,9 @@ namespace BobboNet.SGB.IMod
         private static AudioMixerGroup mixerGroupBGM = null; // (optional) The current mixer group to route BGM through
         private static AudioMixerGroup mixerGroupSFX = null; // (optional) The current mixer group to route SFX through
 
+        private static string mixerVolumeHandleNameBGM = null; // (optional) The name of the BGM's volume handle in it's mixer
+        private static string mixerVolumeHandleNameSFX = null; // (optional) The name of the SFX's volume handle in it's mixer 
+
         //
         //  Constructor
         //
@@ -39,10 +42,12 @@ namespace BobboNet.SGB.IMod
         /// Update what audio mixer group all SGB background music should be routed through.
         /// </summary>
         /// <param name="mixerGroup">The group to route all BGM through. If null, unroutes all BGM.</param>
-        public static void SetMixerGroupBGM(AudioMixerGroup mixerGroup)
+        /// <param name="volumeHandleName">The name of the mixer handle that controls this group's volume level. If null, volume level is ignored.</param>
+        public static void SetMixerGroupBGM(AudioMixerGroup mixerGroup, string volumeHandleName = null)
         {
-            mixerGroupBGM = mixerGroup;         // Update the internal mixer group...
-            Audio.SetMixerGroupBGM(mixerGroup); // ...and apply it to SGB
+            mixerGroupBGM = mixerGroup;                     // Update the internal mixer group...
+            mixerVolumeHandleNameBGM = volumeHandleName;    // ...cache a reference to it's volume handle...
+            Audio.SetMixerGroupBGM(mixerGroup);             // ...and apply it to SGB
         }
 
         /// <summary>
@@ -55,10 +60,12 @@ namespace BobboNet.SGB.IMod
         /// Update what audio mixer group all SGB sound effects should be routed through.
         /// </summary>
         /// <param name="mixerGroup">The group to route all SFX through. If null, unroutes all SFX.</param>
-        public static void SetMixerGroupSFX(AudioMixerGroup mixerGroup)
+        /// <param name="volumeHandleName">The name of the mixer handle that controls this group's volume level. If null, volume level is ignored.</param>
+        public static void SetMixerGroupSFX(AudioMixerGroup mixerGroup, string volumeHandleName = null)
         {
-            mixerGroupSFX = mixerGroup;         // Update the internal mixer group...
-            Audio.SetMixerGroupSFX(mixerGroup); // ...and apply it to SGB
+            mixerGroupSFX = mixerGroup;                     // Update the internal mixer group...
+            mixerVolumeHandleNameSFX = volumeHandleName;    // ...cache a reference to it's volume handle...
+            Audio.SetMixerGroupSFX(mixerGroup);             // ...and apply it to SGB
         }
 
         /// <summary>
@@ -78,6 +85,8 @@ namespace BobboNet.SGB.IMod
         public static void SetVolumeBGMRaw(int volume)
         {
             volumeBGM = Mathf.Clamp(volume, MinVolume, MaxVolume);
+
+            SetMixerVolumeIfNecessary(mixerGroupBGM, mixerVolumeHandleNameBGM, volumeBGM);
             UpdateVolumeState();
         }
 
@@ -107,6 +116,8 @@ namespace BobboNet.SGB.IMod
         public static void SetVolumeSFXRaw(int volume)
         {
             volumeSFX = Mathf.Clamp(volume, MinVolume, MaxVolume);
+
+            SetMixerVolumeIfNecessary(mixerGroupSFX, mixerVolumeHandleNameSFX, volumeSFX);
             UpdateVolumeState();
         }
 
@@ -137,5 +148,24 @@ namespace BobboNet.SGB.IMod
         private static void UpdateVolumeState() => Audio.updateVolume(); // Update the volume in SGB
         private static int ConvertVolumeToInt(float volumeAsFloat) => Mathf.Clamp(Mathf.RoundToInt(volumeAsFloat * 100), MinVolume, MaxVolume);
         private static float ConvertVolumeToFloat(int volumeAsInt) => Mathf.Clamp(volumeAsInt / 100.0f, MinVolume, MaxVolume);
+
+        private static void SetMixerVolumeIfNecessary(AudioMixerGroup mixerGroup, string handleName, int rawVolume)
+        {
+            if (mixerGroup == null || handleName == null) return;
+
+            mixerGroup.audioMixer.SetFloat(handleName, VolumeFloatToDb(ConvertVolumeToFloat(rawVolume)));
+        }
+
+        private static float VolumeFloatToDb(float volumeFloat)
+        {
+            if (volumeFloat != 0)
+            {
+                return 20f * Mathf.Log10(volumeFloat);
+            }
+            else
+            {
+                return -144.0f;
+            }
+        }
     }
 }
